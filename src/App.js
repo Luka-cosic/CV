e7095d28a60af6fa09db16f614e2e03455f2b387
Luka-coisc <luka.cosic2302@gmail.com>
2020-06-16 15:22:53 +0200

    cv

--- src/App.js
+++ src/App.js
@@ -1,26 +1,149 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import "./App.css"
+import { FiPhone } from 'react-icons/fi';
+import { GoMarkGithub } from "react-icons/go";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <div className="github">
+            <div className="row">
+                        <div className="col-2 offset-1">
+                        <h3><GoMarkGithub /></h3>
+                        </div>
+                        <div className="col-8 offset-1">
+                            <a href="https://github.com/Luka-cosic/kafana">GitHub</a>
+                        </div>
+                    </div><hr/>
+            </div>
+            <img src="slike/luka.jpg" alt="" />
+            <div className="contact">
+                <div className="container"><br />
+                    <div className="row">
+                        <div className="col-6 offset-3">
+                            <h5 className="naslov">Contact</h5><hr />
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-2">
+                            <svg className="bi bi-house-door" width="1.5em" height="1.5em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                <path fillRule="evenodd" d="M7.646 1.146a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 .146.354v7a.5.5 0 0 1-.5.5H9.5a.5.5 0 0 1-.5-.5v-4H7v4a.5.5 0 0 1-.5.5H2a.5.5 0 0 1-.5-.5v-7a.5.5 0 0 1 .146-.354l6-6zM2.5 7.707V14H6v-4a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 .5.5v4h3.5V7.707L8 2.207l-5.5 5.5z" />
+                                <path fillRule="evenodd" d="M13 2.5V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z" />
+                            </svg>
+                        </div>
+                        <div className="col-10">
+                            <p>Valjevo,  J.Nenadovica 7/1</p>
+                        </div>
+                    </div><hr />
+                    <div className="row">
+                        <div className="col-2">
+                            <svg className="bi bi-envelope" width="1.5em" height="1.5em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                <path fillRule="evenodd" d="M14 3H2a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4a1 1 0 0 0-1-1zM2 2a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H2z" />
+                                <path d="M.05 3.555C.017 3.698 0 3.847 0 4v.697l5.803 3.546L0 11.801V12c0 .306.069.596.192.856l6.57-4.027L8 9.586l1.239-.757 6.57 4.027c.122-.26.191-.55.191-.856v-.2l-5.803-3.557L16 4.697V4c0-.153-.017-.302-.05-.445L8 8.414.05 3.555z" />
+                            </svg>
+                        </div>
+                        <div className="col-10">
+                            <p>luka.cosic2302@gmail.com</p>
+                        </div>
+                    </div><hr />
+                    <div className="row">
+                        <div className="col-2">
+                        <h3><FiPhone /></h3>
+                        </div>
+                        <div className="col-10">069-2259330</div>
+                    </div><hr/>
+                    <div className="row">
+                        <div className="col-6 naslov">
+                            <h5>Rodjenje:</h5>
+                        </div>
+                        <div className="col-6">
+                            <h6>23.02.1991</h6>
+                        </div>
+                    </div><hr/>
+                    <div className="row naslov">
+                        <div className="col-6">
+                            <h5>Zanimanje:</h5>  
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-12">
+                            <h6>Gradjevinski Tehnicar za Visokogradnju</h6>
+                        </div>
+                    </div>
+                </div><hr/>
+
+
+
+                <div className="row naslov">
+                        <div className="col-10 offset-1 ">
+                            <h5>Radio:</h5>  
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-10 offset-1">
+                            <h6>Kao gradjevinski tehnicar 5 godina</h6>
+                        </div>
+                    </div><hr/>
+                <div className="row">
+                        <div className="col-2 offset-1">
+                        <h3><GoMarkGithub /></h3>
+                        </div>
+                        <div className="col-8 offset-1">
+                            <a href="https://github.com/Luka-cosic/kafana">GitHub</a>
+                        </div>
+                    </div><hr/>
+            </div>
+            <h1>Luka Ćosić</h1>
+            <div className="providno">
+                <div className="row">
+                    <div className="col-10 offset-1">
+                        <h3>Vestine</h3>
+                    </div>
+                    <div className="linija"></div>
+                </div>
+                <p className="text">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Što se tiče programiranja u 
+                proteklih deset meseci svakodnevno vežbam programske jezike PHP  i JavaScript.
+                Na početku svog učenja prvo sam
+                 se upoznao sa HTML- om i CSS- om, da bih kasnije uz pomć online kurseva Danila 
+                Vesovića počeo da učim PHP i JavaScript, objetkno orijentisan nacin pisanja koda na istim
+                 programima, a sa bazom podataka MySql sam komunicirao putem PHP-a i preko JS fajlova uz
+                  pomać  AJAX-a.
+                Sada Trenutno vežbam REACT i NODE praveći aplikacije u tim programima.<br /><br />
+                U linku iznad nalazi se moja prva aplikacija, rađena u REACT-u.</p>
+
+                <div className="row">
+                    <div className="col-10 offset-1">
+                        <h3>Razlog preusmerenja</h3>
+                    </div>
+                    <div className="linija1"></div>
+                </div>
+                <p className="text">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Programiranje sam počeo da učim iz radoznalosti  i
+                 jako brzo sam se zaljubio u ovu profesiju, kada sam počeo da saznajem sta
+                  se desava sa našim podacima kada ih unesemo u neku formu, kao i kako 
+                  funkcionišu sajtovi za kupovinu i prodaju različitih artikala, blogovi i ostalo.
+                  Sve te sajtove sam i sam često koristio i uzbuđivalo me je saznanje kako sve to 
+                  funkcioniše, tako sam pomislio da ne bi bila losa ideja da počnem da se bavim 
+                  poslom koji mi se sviđa.</p>
+
+
+                  <div className="row">
+                    <div className="col-10 offset-1">
+                        <h3>Ambicije</h3>
+                    </div>
+                    <div className="linija3"></div>
+                </div>
+                <p className="text">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Planiram da kroz zaposlenje usavršim rad u REACT-u, NODE-u i PHP-u. 
+                Rad u vašoj firmi bi mi omogućio timski rad sa kojim bi mogao 
+                jos više da napredujem u ovoj
+                 oblasti i na taj nacin obezbedim sigurniju egzistencuju.</p>
+   
+            </div>
+
+            
+            
+            <div className="zanimanje"><h3>Junior Developer</h3></div>
+        </div>
+    )
 }
 
-export default App;
+export default App
